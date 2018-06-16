6afb4f33c5163d011b569d62ca166db5b2800100
Lisa Huynh <lisaahuynh@yahoo.com>
2018-06-16 14:36:45 -0400

    changed server.js file again

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'));
 
 app.use(router);
-require('./routes/routes.js')(router);
+require('./routes/routes')(router);
 
 // handlebars setup
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
