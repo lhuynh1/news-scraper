0cc84f475f5b1c5457a9b13c022d2c38d81e0bbd
Lisa Huynh <lisaahuynh@yahoo.com>
2018-06-18 20:22:28 -0400

    heroku fixes

--- server.js
+++ server.js
@@ -41,8 +41,6 @@ if(process.env.MONGODB_URI == 'production'){
   }
   else{
     mongoose.connect('mongodb://localhost/news-scraper');
-    // YOU CAN IGNORE THE CONNECTION URL BELOW (LINE 41) THAT WAS JUST FOR DELETING STUFF ON A RE-DEPLOYMENT
-    //mongoose.connect('mongodb://heroku_60zpcwg0:ubn0n27pi2856flqoedo9glvh8@ds119578.mlab.com:19578/heroku_60zpcwg0');
   };
 
 db.on('error', function(err) {
