d7d643294017bbcc3ed74b0c595c6effc25c390f
Lisa Huynh <lisaahuynh@yahoo.com>
2018-06-18 20:14:34 -0400

    server fixes

--- server.js
+++ server.js
@@ -29,12 +29,25 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 // mongoose connection
-mongoose.connect('mongodb://heroku_5lvr0ftp:e0e2m50qkmoqkijn0fkbasbpp8@ds259620.mlab.com:59620/heroku_5lvr0ftp');
-if (process.env.MONGODB_URI){
-        mongoose.connect(process.env.MONGODB_URI);
-} else {
-    mongoose.connect('mongodb://localhost/news-scraper')
-}
+// mongoose.connect('mongodb://heroku_5lvr0ftp:e0e2m50qkmoqkijn0fkbasbpp8@ds259620.mlab.com:59620/heroku_5lvr0ftp');
+// if (process.env.MONGODB_URI){
+//         mongoose.connect(process.env.MONGODB_URI);
+// } else {
+//     mongoose.connect('mongodb://localhost/news-scraper')
+// };
+
+if(process.env.NODE_ENV == 'production'){
+    mongoose.connect('mongodb://heroku_5lvr0ftp:e0e2m50qkmoqkijn0fkbasbpp8@ds259620.mlab.com:59620/heroku_5lvr0ftp');
+  }
+  else{
+    mongoose.connect('mongodb://localhost/news-scraper');
+    // YOU CAN IGNORE THE CONNECTION URL BELOW (LINE 41) THAT WAS JUST FOR DELETING STUFF ON A RE-DEPLOYMENT
+    //mongoose.connect('mongodb://heroku_60zpcwg0:ubn0n27pi2856flqoedo9glvh8@ds119578.mlab.com:19578/heroku_60zpcwg0');
+  };
+
+db.on('error', function(err) {
+    console.log('Mongoose Error: ', err);
+  });
 
 db.once('open', function() {
     console.log('Mongoose is connected!')
